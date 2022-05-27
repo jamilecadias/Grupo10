83cb674d27d1fa4017c3cfb9e05dc74f88c344c7
AlejoMoro <alejandromontaro@mail.com>
2022-05-26 23:52:35 -0500

    Iniciando Home

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+const publicPath= path.resolve(__dirname, './public')
+app.use(express.static(publicPath));
+app.get('/', (req, res)=>{
+    res.sendFile (path.join(__dirname,'/views/home.html'))
+});
+app.listen (3000, (req, res)=> console.log ('Servidor 3000 funcionando'));
