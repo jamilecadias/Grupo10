b6b191740aa80bdcb7facb2858a51a051cc7d649
jamilecadias <jamilecadias@gmail.com>
2022-05-28 12:52:38 -0300

    Creando una ruta registro en app.js

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require ('express');
 const path = require ('path');
 const app = express ();
 const publicPath= path.resolve(__dirname, './public')
+
 app.use(express.static(publicPath));
+
 app.get('/', (req, res)=>{
     res.sendFile (path.join(__dirname,'/views/home.html'))
+});
+app.get('/', (req, res)=>{
+    res.sendFile (path.join(__dirname,'/views/registro.html'))
 });
 app.listen (3000, (req, res)=> console.log ('Servidor 3000 funcionando'));
