9bc0347560945c1d829ba5ba928855e50e10bd1e
nicobjk <nicolasbojko14@hotmail.com>
2022-07-09 23:39:50 -0300

    implementando multer

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,7 +1,21 @@
 const express = require ('express');
 const router = express.Router();  
 const mainController = require('../controllers/mainController'); 
+const path = require('path');
+
+// Multer
 const multer = require('multer'); 
+const storageUsers = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, path.join(__dirname, '../public/images/usersImages'))
+    },
+    filename:(req, file, cb)=>{
+        let userName = "user-" + Date.now() + path.extname(file.originalname);
+        cb(null, userName); 
+    }
+
+})
+const uploadUser = multer({ storage : storageUsers });
 
 // Acceso al home
 router.get('/', mainController.home);
@@ -15,11 +29,11 @@ router.get('/products', mainController.listaProductos);
 router.get('/products/:id', mainController.producto);
 
 // Formulario de creación
-router.get('/products/create', mainController.cargar); // Acceso
+router.get('/products/create', mainController.cargar); // Acceso vista
 router.post('/products', mainController.store); // Procesamiento
 
 // Formulario de edición
-router.get('/products/:id/edit', mainController.edit); // Acceso
+router.get('/products/:id/edit', mainController.edit); // Acceso vista
 router.put('/products/:id', mainController.update);  //Envío
 router.delete('/products/:id', mainController.destroy); // Eliminar
 
@@ -28,8 +42,8 @@ router.delete('/products/:id', mainController.destroy); // Eliminar
 router.get('/login', mainController.login);
 
 // Registro
-router.get('/register', mainController.register); // Acceso
-router.post('/register' , mainController.processUsersRegister) // Procesamiento
+router.get('/register', mainController.register); // Acceso vista
+router.post('/register' , uploadUser.single('profile-image') ,mainController.processUsersRegister) // Procesamiento
 
 // Acceso al carrito
 router.get('/iconoCarrito', mainController.carrito);
