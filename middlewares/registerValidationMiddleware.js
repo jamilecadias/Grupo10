f0dd53b47394eea818c322e5f2074e87153600a1
belenazz <kixz.bg@gmail.com>
2022-08-28 19:39:08 -0300

    identacion en validation

--- middlewares/registerValidationMiddleware.js
+++ middlewares/registerValidationMiddleware.js
@@ -2,12 +2,22 @@ const path = require('path');
 const { body } = require('express-validator'); 
 
 const validations = [
-    body('name').notEmpty().withMessage('Este campo debe estar completo.'),
-    body('email').notEmpty().withMessage('Este campo debe estar completo.').bail()
-    .isEmail().withMessage('Debe ser un email valido.'),
+    body('name')
+        .notEmpty()
+        .withMessage('Este campo debe estar completo.'),
+    body('email')
+        .notEmpty()
+        .withMessage('Este campo debe estar completo.')
+        .bail()
+        .isEmail()
+        .withMessage('Ingresar un email válido'),
     //body('tel').isLength({ min: 8, max:12 }).withMessage('Debe tener entre 8 y 12 digitos.'),//
-    body('password').notEmpty().withMessage('Dede ingresar una contraseña').bail()
-    .isLength(({ min: 8 })).withMessage('La contraseña debe tener al menos 8 caracteres.'),
+    body('password')
+        .notEmpty()
+        .withMessage('Dede ingresar una contraseña')
+        .bail()
+        .isLength(({ min: 8 }))
+        .withMessage('La contraseña debe tener al menos 8 caracteres.'),
     body('avatar').custom((value , {req}) => {
         let file = req.file; 
         let acceptedExtensions = ['.jpg' , '.gif' , '.png', '.jpeg'];
